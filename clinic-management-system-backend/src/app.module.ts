872ba03dea95aec864d365b496737d5c484f15ec
Arthur Clores <arthurclores@gmail.com>
2023-07-15 19:11:58 +0700

    CMS-B-002- Create a Secure authentication and authorization mechanism- DONE

--- clinic-management-system-backend/src/app.module.ts
+++ clinic-management-system-backend/src/app.module.ts
@@ -1,12 +1,29 @@
-import { PostgresDatabaseProviderModule } from '@/providers/database/postgres/provider.module';
-import { PostgresDatabaseConfigModule } from '@/config/database/postgres/config.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [PostgresDatabaseConfigModule, PostgresDatabaseProviderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      })
+    }),
+    DatabaseModule,
+    AuthModule,
+    UserModule
+    ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ clinic-management-system-backend/src/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string;
+
+    @Column()
+    role: string;
+
+    }
