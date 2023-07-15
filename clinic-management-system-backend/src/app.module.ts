461efffd8a327aff975bb396b65c72640b403f1d
alwyn tablatin <alwyn.tablatin@gmail.com>
2023-07-15 12:26:32 +0800

    feat: add database connection

--- clinic-management-system-backend/src/app.module.ts
+++ clinic-management-system-backend/src/app.module.ts
@@ -1,9 +1,11 @@
+import { PostgresDatabaseProviderModule } from '@/providers/database/postgres/provider.module';
+import { PostgresDatabaseConfigModule } from '@/config/database/postgres/config.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PostgresDatabaseConfigModule, PostgresDatabaseProviderModule],
   controllers: [AppController],
   providers: [AppService],
 })
